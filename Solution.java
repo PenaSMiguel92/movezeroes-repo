81cd9e72a0df1b29c0e783f531fd5f411be5e6cb
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 21:52:49 -0700

    faster implementation

--- Solution.java
+++ Solution.java
@@ -2,37 +2,15 @@
 public class Solution {
 
     public void moveZeroes(int[] nums) {
-        int zeroIndex = nums.length - 1;
-        int currentIndex = zeroIndex - 1;
-        int zeroCount = 0;
-        for (int num : nums) {
-            if (num == 0)
-                zeroCount++;
-        }
-        if (zeroCount <= 0)
-            return;
-        if (zeroCount == nums.length)
-            return;
-        while (zeroIndex >= nums.length - zeroCount) {
-            if (zeroIndex - 1 >= 0) {
-                if (nums[zeroIndex] == 0) {
-                    zeroIndex--;
-                    currentIndex = zeroIndex - 1;
-                } else if (nums[zeroIndex] != 0 && nums[currentIndex] == 0) {
-                    for (int i = currentIndex; i < zeroIndex; i++) {
-                        nums[i] = nums[i + 1];
-                        nums[i + 1] = 0;
-                    }
-                    zeroIndex--;
-                    currentIndex = zeroIndex - 1;
-                }
-            } else {
-                break;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
             }
-                
-           
-            currentIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : zeroIndex - 1;
-            
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
         }
     }
 }
