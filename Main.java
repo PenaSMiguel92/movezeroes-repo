77788b118be4422aef6d2e4ac9010544b92f4e95
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 20:06:35 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,4 +1,16 @@
+import java.util.*;
+
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] arr1 = new int[] { 0, 1, 0, 3, 12 };
+        int[] arr2 = new int[] { 0 };
+
+        sol.moveZeroes(arr1);
+        sol.moveZeroes(arr2);
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
 } 
