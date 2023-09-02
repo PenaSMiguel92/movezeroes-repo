05c0a36cb8af03b63b21caab46f47c6be2ab72c8
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 21:04:24 -0700

    correct solution found

--- Solution.java
+++ Solution.java
@@ -1,12 +1,10 @@
-import java.util.*;
 
 public class Solution {
 
     public void moveZeroes(int[] nums) {
-        int currentIndex = 0;
-        int zeroIndex = -1;
+        int zeroIndex = nums.length - 1;
+        int currentIndex = zeroIndex - 1;
         int zeroCount = 0;
-        boolean sorted = false;
         for (int num : nums) {
             if (num == 0)
                 zeroCount++;
@@ -15,30 +13,19 @@ public class Solution {
             return;
         if (zeroCount == nums.length)
             return;
-        while (!sorted) {
-            System.out.println(Arrays.toString(nums));
-            if (nums[currentIndex] == 0)
-                zeroIndex = currentIndex;
-            if ((nums[currentIndex] != 0) && (zeroIndex != -1)) {
-                nums[zeroIndex] = nums[currentIndex];
-                nums[currentIndex] = 0;
-                zeroIndex = -1;
+        while (zeroIndex >= nums.length - zeroCount) {
+            if (nums[zeroIndex] == 0) {
+                zeroIndex--;
+                currentIndex = zeroIndex - 1;
             }
-
-            for (int i = nums.length - zeroCount; i < nums.length; i++) {
-                if (nums[i] != 0) {
-                    sorted = false;
-                    break;
-                }
-
-                if (i == nums.length - 1 && nums[i] == 0)
-                    sorted = true;
+            if (nums[currentIndex] == 0 && nums[zeroIndex] != 0) {
+                nums[currentIndex] = nums[zeroIndex];
+                nums[zeroIndex] = 0;
+                zeroIndex--;
+                currentIndex = zeroIndex - 1;
             }
-
-            if (currentIndex + 1 >= nums.length)
-                currentIndex = 0;
-            else
-                currentIndex++;
+            currentIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : zeroIndex - 1;
+            
         }
     }
 }
